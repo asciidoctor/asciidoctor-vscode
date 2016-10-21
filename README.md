d9a6e854791ff65d8c68721726f053899bd59e10
João Pinto <lamego.pinto@gmail.com>
2016-10-21 21:09:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# AsciiDoc Previewer
+
+An extension to preview AsciiDoc text using the _AsciiDoctor_ publishing tool.
+
+The extension can be activate in two ways
+
+* Toggle Preview - `ctrl+shift+r`
+* Open Preview to the Side - `ctrl+k r`
+
+![alt](images/simple.png)
+
+
+## Prerequisites
+
+You need to [**install AsciiDoctor**](http://asciidoctor.org/docs/install-toolchain/ ) - A fast text processor & publishing toolchain for converting AsciiDoc to HTML5, DocBook & more.
+
+## How to build and install from source (Linux)
+```
+git clone https://github.com/joaompinto/vscode-instant-asciidoc
+cd vscode-instant-asciidoc
+npm install
+sudo npm install vsce
+vsce package
+code --install-extension *.vsix
+```
+
+## Credits:
+This extension code was based on https://github.com/tht13/RST-vscode/
---
+++ src/extension.ts
@@ -0,0 +1,160 @@
+"use strict";
+import { workspace, window, ExtensionContext, commands,
+TextEditor, TextDocumentContentProvider, EventEmitter,
+Event, Uri, TextDocumentChangeEvent, ViewColumn,
+TextEditorSelectionChangeEvent,
+TextDocument, Disposable } from "vscode";
+import { exec } from "child_process";
+import * as fs from "fs";
+import * as path from "path";
+let fileUrl = require("file-url");
+let tmp = require("tmp");
+
+export function activate(context: ExtensionContext) {
+
+    let previewUri: Uri;
+
+    let provider: ADocDocumentContentProvider;
+    let registration: Disposable;
+
+    workspace.onDidChangeTextDocument((e: TextDocumentChangeEvent) => {
+        if (e.document === window.activeTextEditor.document) {
+            provider.update(previewUri);
+        }
+    });
+
+    workspace.onDidSaveTextDocument((e: TextDocument) => {
+        if (e === window.activeTextEditor.document) {
+            provider.update(previewUri);
+        }
+    });
+
+    function sendHTMLCommand(displayColumn: ViewColumn): PromiseLike<void> {
+        let previewTitle = `Preview: '${path.basename(window.activeTextEditor.document.fileName)}'`;
+        provider = new ADocDocumentContentProvider();
+        registration = workspace.registerTextDocumentContentProvider("adoc-preview", provider);
+        previewUri = Uri.parse(`adoc-preview://preview/${previewTitle}`);
+        return commands.executeCommand("vscode.previewHtml", previewUri, displayColumn).then((success) => {
+        }, (reason) => {
+            console.warn(reason);
+            window.showErrorMessage(reason);
+        });
+    }
+
+    let previewToSide = commands.registerCommand("adoc.previewToSide", () => {
+        let displayColumn: ViewColumn;
+        switch (window.activeTextEditor.viewColumn) {
+            case ViewColumn.One:
+                displayColumn = ViewColumn.Two;
+                break;
+            case ViewColumn.Two:
+            case ViewColumn.Three:
+                displayColumn = ViewColumn.Three;
+                break;
+        }
+        return sendHTMLCommand(displayColumn);
+    });
+
+    let preview = commands.registerCommand("adoc.preview", () => {
+        return sendHTMLCommand(window.activeTextEditor.viewColumn);
+    });
+
+    context.subscriptions.push(previewToSide, preview, registration);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
+class ADocDocumentContentProvider implements TextDocumentContentProvider {
+    private _onDidChange = new EventEmitter<Uri>();
+    private resultText = "";
+
+    public provideTextDocumentContent(uri: Uri): string | Thenable<string> {
+        return this.createADocSnippet();
+    }
+
+    get onDidChange(): Event<Uri> {
+        return this._onDidChange.event;
+    }
+
+    public update(uri: Uri) {
+        this._onDidChange.fire(uri);
+    }
+
+    private createADocSnippet(): string | Thenable<string> {
+        let editor = window.activeTextEditor;
+        if (!(editor.document.languageId === "asciidoc")) {
+            return this.errorSnippet("Active editor doesn't show an AsciiDoc document - no properties to preview.");
+        }
+        return this.preview(editor);
+    }
+
+    private errorSnippet(error: string): string {
+        return `
+                <body>
+                    ${error}
+                </body>`;
+    }
+
+    private buildPage(document: string): string {
+        return document;
+    }
+
+    private createStylesheet(file: string) {
+        let href = fileUrl(
+            path.join(
+                __dirname,
+                "..",
+                "..",
+                "src",
+                "static",
+                file
+            )
+        );
+        return `<link href="${href}" rel="stylesheet" />`;
+    }
+
+    private fixLinks(document: string, documentPath: string): string {
+        return document.replace(
+            new RegExp("((?:src|href)=[\'\"])(.*?)([\'\"])", "gmi"), (subString: string, p1: string, p2: string, p3: string): string => {
+                return [
+                    p1,
+                    fileUrl(path.join(
+                        path.dirname(documentPath),
+                        p2
+                    )),
+                    p3
+                ].join("");
+            }
+        );
+    }
+
+    public preview(editor: TextEditor): Thenable<string> {
+        let doc = editor.document;
+        return new Promise<string>((resolve, reject) => {
+            let text = doc.getText();
+            let documentPath = path.dirname(editor.document.fileName);
+            let tmpobj = tmp.fileSync({postfix: '.adoc', dir: documentPath});
+            let cmd = workspace.getConfiguration('AsciiDoc').get('html_generator') +" " +tmpobj.name;
+            fs.write(tmpobj.fd, text, 0); 
+            exec(cmd, (error: Error, stdout: Buffer, stderr: Buffer) => {
+                tmpobj.removeCallback();
+                if (error) {
+                    let errorMessage = [
+                        error.name,
+                        error.message,
+                        error.stack,
+                        "",
+                        stderr.toString()
+                    ].join("\n");
+                    console.error(errorMessage);
+                    reject(errorMessage);
+                } else {
+                    let result = this.fixLinks(stdout.toString(), editor.document.fileName);
+                    resolve(this.buildPage(result));
+                }
+            });
+        });
+    }
+}
