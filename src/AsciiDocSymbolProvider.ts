0a1b3730c8b14b4c2ff54d36a97ee32ebb40dcda
Stephen M. Coakley <me@stephencoakley.com>
2018-09-22 15:56:17 -0500

    Add support for tree-like outline
    Use the new `DocumentSymbol` API to provide a hierarchial symbol tree in order to have a more useful outline for navigation.
    
    Closes issue #95.

--- src/AsciiDocSymbolProvider.ts
+++ src/AsciiDocSymbolProvider.ts
@@ -1,35 +1,77 @@
 import {
     languages,
-    Disposable,
     CancellationToken,
-    Location,
-    Position,
-    TextDocument,
-    SymbolInformation,
-    SymbolKind
+    Disposable,
+    DocumentSymbol,
+    SymbolKind,
+    TextDocument
 } from 'vscode';
 
 export default function registerDocumentSymbolProvider(): Disposable {
-    
-        const _atxPattern = /^(=|#){1,6}\s+.+/;
+
+        const _atxPattern = /^((=|#){1,6})\s+(.+)$/;
+        const _blockPattern = /^[.\-]{4}$/;
 
         return languages.registerDocumentSymbolProvider('asciidoc', {
-    
-            provideDocumentSymbols(document: TextDocument, token: CancellationToken): SymbolInformation[] {
-    
-                const result: SymbolInformation[] = [];
+
+            provideDocumentSymbols(document: TextDocument, token: CancellationToken): DocumentSymbol[] {
+
+                const symbols: DocumentSymbol[] = [];
                 const lineCount = Math.min(document.lineCount, 10000);
+
+                // Really basic way of ignoring literal blocks when parsing headers.
+                let currentBlock: string = null;
+
                 for (let line = 0; line < lineCount; line++) {
-                    const {text} = document.lineAt(line);
-    
-                    if (_atxPattern.test(text)) {
-                        // atx-style, 1-6 = characters
-                        result.push(new SymbolInformation(text, SymbolKind.File, '',
-                            new Location(document.uri, new Position(line, 0))));
+                    const {text, range} = document.lineAt(line);
+
+                    if (_blockPattern.test(text)) {
+                        // Entering a block.
+                        if (currentBlock === null) {
+                            currentBlock = text;
+                        }
+
+                        // Exiting a block.
+                        else if (text === currentBlock) {
+                            currentBlock = null;
+                        }
+
+                        continue;
+                    }
+
+                    // Ignore lines in a literal block.
+                    if (currentBlock !== null) {
+                        continue;
+                    }
+
+                    const match = _atxPattern.exec(text);
+                    if (match !== null) {
+                        const depth = match[1].length;
+                        const symbol = new DocumentSymbol(
+                            match[3],
+                            null,
+                            SymbolKind.String,
+                            range,
+                            range
+                        );
+
+                        let parent = symbols;
+
+                        for (let current = 1; current < depth; current++) {
+                            // Some documents are not well-formed; promote this heading to a higher level if that is the
+                            // case.
+                            if (parent.length === 0) {
+                                break;
+                            }
+
+                            parent = parent[parent.length - 1].children;
+                        }
+
+                        parent.push(symbol);
                     }
                 }
-    
-                return result;
+
+                return symbols;
             }
         });
     }
