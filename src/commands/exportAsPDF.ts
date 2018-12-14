271a3430dc5439f8b688474531d6c071925c6c5b
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-12-14 01:59:28 +0100

    exportAsPDF - add option to use asciidoctor-pdf instead wkhtmltopdf

--- src/commands/exportAsPDF.ts
+++ src/commands/exportAsPDF.ts
@@ -29,92 +29,149 @@ export class ExportAsPDF implements Command {
         const doc = editor.document
         const text = doc.getText()
 
-        let parser = new AsciiDocParser(path.resolve(doc.fileName))
-        //const body =  await parser.parseText()
-        const body = await this.engine.render(doc.uri, true, text)
-        const ext_path = vscode.extensions.getExtension('joaompinto.asciidoctor-vscode').extensionPath;
-        const html = body;
-        const showtitlepage = parser.getAttribute("showtitlepage")
-        const author = parser.getAttribute("author")
-        const email = parser.getAttribute("email")
-        const doctitle : string | undefined = parser.getAttribute("doctitle");
-        const titlepagelogo : string | undefined = parser.getAttribute("titlepagelogo");
-        const footer_center: string | undefined = parser.getAttribute("footer-center");
-        const source_name = path.parse(path.resolve(doc.fileName))
-        let cover: string | undefined = undefined;
-        let img_html: string = '';
-        if(!isNullOrUndefined(showtitlepage)) {
-            if(!isNullOrUndefined(titlepagelogo)) {
-                const image_url = titlepagelogo.startsWith('http') ? titlepagelogo : path.join(source_name.dir, titlepagelogo)
-                img_html = isNullOrUndefined(titlepagelogo) ? "" : `<img src="${image_url}">`
+        if (vscode.workspace.getConfiguration('asciidoc').get('use_asciidoctorpdf')) {
+            var docPath = path.parse(path.resolve(doc.fileName))
+            var pdfPath = ''
+
+            if (doc.isUntitled) {
+                pdfPath = path.join(docPath.root, docPath.dir, "temp.pdf")
+            } else {
+                pdfPath = path.join(docPath.root, docPath.dir, docPath.name+".pdf")
             }
-            var tmpobj = tmp.fileSync({postfix: '.html'});
-            let html =  `\
-            <!DOCTYPE html>
-            <html>
-                <head>
-                <meta charset="UTF-8">
-                <link rel="stylesheet" type="text/css" href="${ext_path + "/media/all-centered.css"}">
-                </head>
-                <body>
-                <div class="outer">
-                    <div class="middle">
-                        <div class="inner">
-                            ${img_html}
-                            <h1>${doctitle}</h1>
-                            <p>${author} &lt;${email}&gt;</p>
+
+            var pdfUri = await vscode.window.showSaveDialog({ defaultUri: vscode.Uri.file(pdfPath) })
+            if (!isNullOrUndefined(pdfUri)) {
+                pdfPath = pdfUri.fsPath
+            } else {
+                console.error(`ERROR: invalid pdfUri "${pdfUri}"`)
+                return
+            }
+
+            let asciidoctorpdf_command = vscode.workspace
+                .getConfiguration('asciidoc')
+                .get('asciidoctorpdf_command', 'asciidoctor-pdf')
+
+            var adocpdf_cmd_array = asciidoctorpdf_command
+                .split(/(\s+)/)
+                .filter( function(e) { return e.trim().length > 0 } )
+
+            var adocpdf_cmd = adocpdf_cmd_array[0]
+
+            var adocpdf_cmd_args = adocpdf_cmd_array.slice(1)
+            adocpdf_cmd_args.push.apply(adocpdf_cmd_args, ['-q', '-o-', '-',
+                '-B', '"' + docPath.dir.replace('"', '\\"') + '"',
+                '-o', '"' + pdfPath.replace('"', '\\"') + '"'
+            ])
+
+            var options = { shell: true, cwd: docPath.dir }
+
+            var asciidoctorpdf = spawn(adocpdf_cmd, adocpdf_cmd_args, options)
+
+            asciidoctorpdf.stderr.on('data', (data) => {
+                let errorMessage = data.toString()
+                errorMessage += "\n"
+                errorMessage += "command: " + adocpdf_cmd + " " + adocpdf_cmd_args.join(" ")
+                errorMessage += "\n"
+                errorMessage += "If the asciidoctor-pdf binary is not in your PATH, you can set the full path."
+                errorMessage += "Go to `File -> Preferences -> User settings` and adjust the asciidoc.asciidoctorpdf_command"
+                console.error(errorMessage)
+                vscode.window.showErrorMessage(errorMessage)
+            })
+
+            asciidoctorpdf.on('close', (code) => {
+                vscode.window.showInformationMessage(`${path.parse(pdfPath).base} has been saved`)
+            })
+
+            asciidoctorpdf.stdin.write(text)
+            asciidoctorpdf.stdin.end()
+        } else {
+            let parser = new AsciiDocParser(path.resolve(doc.fileName))
+            //const body =  await parser.parseText()
+            const body = await this.engine.render(doc.uri, true, text)
+            const ext_path = vscode.extensions.getExtension('joaompinto.asciidoctor-vscode').extensionPath;
+            const html = body;
+            const showtitlepage = parser.getAttribute("showtitlepage")
+            const author = parser.getAttribute("author")
+            const email = parser.getAttribute("email")
+            const doctitle : string | undefined = parser.getAttribute("doctitle");
+            const titlepagelogo : string | undefined = parser.getAttribute("titlepagelogo");
+            const footer_center: string | undefined = parser.getAttribute("footer-center");
+            const source_name = path.parse(path.resolve(doc.fileName))
+            let cover: string | undefined = undefined;
+            let img_html: string = '';
+            if(!isNullOrUndefined(showtitlepage)) {
+                if(!isNullOrUndefined(titlepagelogo)) {
+                    const image_url = titlepagelogo.startsWith('http') ? titlepagelogo : path.join(source_name.dir, titlepagelogo)
+                    img_html = isNullOrUndefined(titlepagelogo) ? "" : `<img src="${image_url}">`
+                }
+                var tmpobj = tmp.fileSync({postfix: '.html'});
+                let html =  `\
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                    <meta charset="UTF-8">
+                    <link rel="stylesheet" type="text/css" href="${ext_path + "/media/all-centered.css"}">
+                    </head>
+                    <body>
+                    <div class="outer">
+                        <div class="middle">
+                            <div class="inner">
+                                ${img_html}
+                                <h1>${doctitle}</h1>
+                                <p>${author} &lt;${email}&gt;</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                </body>
-            </html>
-            `;
-            fs.writeFileSync(tmpobj.name, html, 'utf-8')
-            cover = `cover ${tmpobj.name}`;
-        }
-        const platform = process.platform
-        const ext = platform == "win32" ? '.exe': ''
-        const arch = process.arch;
-        var binary_path = path.resolve(path.join(__dirname, 'wkhtmltopdf-'+platform+'-'+arch+ext))
-        const pdf_filename = vscode.Uri.file(path.join(source_name.root, source_name.dir, source_name.name+'.pdf'))
-        if(!fs.existsSync(binary_path) ) {
-            var label = await vscode.window.showInformationMessage("This feature requires wkhtmltopdf\ndo you want to download", "Download")
-            if (label != "Download")
-                return
-            var error_msg = null
-
-            await vscode.window.withProgress({
-                location: vscode.ProgressLocation.Window,
-                title: "Downloading wkhtmltopdf",
-                // cancellable: true
-            }, async(progress) => {
-                progress.report({ message: 'Downloading wkhtmltopdf...'});
-                const download_url = `https://github.com/asciidoctor/asciidoctor-vscode/raw/master/wkhtmltopdf-bin/wkhtmltopdf-${platform}-${arch}${ext}.gz`
-                await download_file(download_url, binary_path+".gz", progress).then( () => {
-                    progress.report({ message: 'Unzipping wkhtmltopdf...'})
-                    const ungzip = zlib.createGunzip()
-                    const inp = fs.createReadStream(binary_path+".gz")
-                    const out = fs.createWriteStream(binary_path)
-                    inp.pipe(ungzip).pipe(out)
-                    fs.chmodSync(binary_path, 0x755)
-                }).catch( async(reason) => {
-                    binary_path = null;
-                    console.error("Error downloading", download_url, " ", reason)
-                    await vscode.window.showErrorMessage("Error installing wkhtmltopdf, "+reason.toString())
+                    </body>
+                </html>
+                `;
+                fs.writeFileSync(tmpobj.name, html, 'utf-8')
+                cover = `cover ${tmpobj.name}`;
+            }
+            const platform = process.platform
+            const ext = platform == "win32" ? '.exe': ''
+            const arch = process.arch;
+            var binary_path = path.resolve(path.join(__dirname, 'wkhtmltopdf-'+platform+'-'+arch+ext))
+            const pdf_filename = vscode.Uri.file(path.join(source_name.root, source_name.dir, source_name.name+'.pdf'))
+            if(!fs.existsSync(binary_path) ) {
+                var label = await vscode.window.showInformationMessage("This feature requires wkhtmltopdf\ndo you want to download", "Download")
+                if (label != "Download")
                     return
+                var error_msg = null
+
+                await vscode.window.withProgress({
+                    location: vscode.ProgressLocation.Window,
+                    title: "Downloading wkhtmltopdf",
+                    // cancellable: true
+                }, async(progress) => {
+                    progress.report({ message: 'Downloading wkhtmltopdf...'});
+                    const download_url = `https://github.com/asciidoctor/asciidoctor-vscode/raw/master/wkhtmltopdf-bin/wkhtmltopdf-${platform}-${arch}${ext}.gz`
+                    await download_file(download_url, binary_path+".gz", progress).then( () => {
+                        progress.report({ message: 'Unzipping wkhtmltopdf...'})
+                        const ungzip = zlib.createGunzip()
+                        const inp = fs.createReadStream(binary_path+".gz")
+                        const out = fs.createWriteStream(binary_path)
+                        inp.pipe(ungzip).pipe(out)
+                        fs.chmodSync(binary_path, 0x755)
+                    }).catch( async(reason) => {
+                        binary_path = null;
+                        console.error("Error downloading", download_url, " ", reason)
+                        await vscode.window.showErrorMessage("Error installing wkhtmltopdf, "+reason.toString())
+                        return
+                    })
                 })
-            })
-            if(isNullOrUndefined(binary_path))
-                return;
-        }
-        var save_filename = await vscode.window.showSaveDialog({ defaultUri: pdf_filename})
-        if(!isNullOrUndefined(save_filename)) {
-            html2pdf(html, binary_path, cover, footer_center, save_filename.fsPath)
-            .then((result) => { offer_open(result) })
-            .catch(reason => {
-                console.error("Got error", reason)
-                vscode.window.showErrorMessage("Error converting to PDF, "+reason.toString());
-            })
+                if(isNullOrUndefined(binary_path))
+                    return;
+            }
+            var save_filename = await vscode.window.showSaveDialog({ defaultUri: pdf_filename})
+            if(!isNullOrUndefined(save_filename)) {
+                html2pdf(html, binary_path, cover, footer_center, save_filename.fsPath)
+                .then((result) => { offer_open(result) })
+                .catch(reason => {
+                    console.error("Got error", reason)
+                    vscode.window.showErrorMessage("Error converting to PDF, "+reason.toString());
+                })
+            }
         }
     }
 }
