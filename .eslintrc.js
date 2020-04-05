fa53a50b400aafa5d8e158672dabbb5338e29812
larshp <larshp@hotmail.com>
2020-04-05 08:15:58 +0200

    add basic setup, remove tslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "root": true,
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": "tsconfig.json",
+    "tsconfigRootDir": __dirname,
+    "sourceType": "module"
+  },
+  "extends": [
+//    "eslint:recommended",
+//    "plugin:@typescript-eslint/recommended",
+  ],
+  "rules": {
+    "arrow-parens": ["error", "always"],
+    /*
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline"
+    }],
+    */
+    "max-len": ["error", {
+      "code": 160,
+      "ignoreStrings": true,
+      "ignoreUrls": true,
+      "ignoreTemplateLiterals": true
+    }],
+    "spaced-comment": "off"
+  }
+};
