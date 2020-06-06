b5d6479e691228888022504cc1ae6f1be90d8222
João Pinto <lamego.pinto@gmail.com>
2020-06-06 14:51:30 +0200

    Reformat indentation

--- src/features/previewManager.ts
+++ src/features/previewManager.ts
@@ -12,7 +12,8 @@ import { AsciidocPreviewConfigurationManager } from './previewConfig';
 import { AsciidocContentProvider } from './previewContentProvider';
 
 
-export class AsciidocPreviewManager implements vscode.WebviewPanelSerializer {
+export class AsciidocPreviewManager implements vscode.WebviewPanelSerializer
+{
 	private static readonly asciidocPreviewActiveContextKey = 'asciidocPreviewFocus';
 
 	private readonly _topmostLineMonitor = new AsciidocFileTopmostLineMonitor();
@@ -25,130 +26,153 @@ export class AsciidocPreviewManager implements vscode.WebviewPanelSerializer {
 		private readonly _contentProvider: AsciidocContentProvider,
 		private readonly _logger: Logger,
 		private readonly _contributions: AsciidocContributions
-	) {
-	  this._disposables.push(vscode.window.registerWebviewPanelSerializer(AsciidocPreview.viewType, this));
+	)
+	{
+		this._disposables.push(vscode.window.registerWebviewPanelSerializer(AsciidocPreview.viewType, this));
 	}
 
-	public dispose(): void {
-	  disposeAll(this._disposables);
-	  disposeAll(this._previews);
+	public dispose(): void
+	{
+		disposeAll(this._disposables);
+		disposeAll(this._previews);
 	}
 
-	public refresh() {
-	  for (const preview of this._previews) {
-	    preview.refresh();
-	  }
+	public refresh()
+	{
+		for (const preview of this._previews)
+		{
+			preview.refresh();
+		}
 	}
 
-	public updateConfiguration() {
-	  for (const preview of this._previews) {
-	    preview.updateConfiguration();
-	  }
+	public updateConfiguration()
+	{
+		for (const preview of this._previews)
+		{
+			preview.updateConfiguration();
+		}
 	}
 
 	public preview(
-	  resource: vscode.Uri,
-	  previewSettings: PreviewSettings
-	): void {
-	  let preview = this.getExistingPreview(resource, previewSettings);
-	  if (preview) {
-	    preview.reveal(previewSettings.previewColumn);
-	  } else {
-	    preview = this.createNewPreview(resource, previewSettings);
-	  }
+		resource: vscode.Uri,
+		previewSettings: PreviewSettings
+	): void
+	{
+		let preview = this.getExistingPreview(resource, previewSettings);
+		if (preview)
+		{
+			preview.reveal(previewSettings.previewColumn);
+		} else
+		{
+			preview = this.createNewPreview(resource, previewSettings);
+		}
 
-	  preview.update(resource);
+		preview.update(resource);
 	}
 
-	public get activePreviewResource() {
-	  return this._activePreview && this._activePreview.resource;
+	public get activePreviewResource()
+	{
+		return this._activePreview && this._activePreview.resource;
 	}
 
-	public toggleLock() {
-	  const preview = this._activePreview;
-	  if (preview) {
-	    preview.toggleLock();
+	public toggleLock()
+	{
+		const preview = this._activePreview;
+		if (preview)
+		{
+			preview.toggleLock();
 
-	    // Close any previews that are now redundant, such as having two dynamic previews in the same editor group
-	    for (const otherPreview of this._previews) {
-	      if (otherPreview !== preview && preview.matches(otherPreview)) {
-	        otherPreview.dispose();
-	      }
-	    }
-	  }
+			// Close any previews that are now redundant, such as having two dynamic previews in the same editor group
+			for (const otherPreview of this._previews)
+			{
+				if (otherPreview !== preview && preview.matches(otherPreview))
+				{
+					otherPreview.dispose();
+				}
+			}
+		}
 	}
 
 	public async deserializeWebviewPanel(
-	  webview: vscode.WebviewPanel,
-	  state: any
-	): Promise<void> {
-	  const preview = await AsciidocPreview.revive(
-	    webview,
-	    state,
-	    this._contentProvider,
-	    this._previewConfigurations,
-	    this._logger,
-	    this._topmostLineMonitor,
-	    this._contributions);
+		webview: vscode.WebviewPanel,
+		state: any
+	): Promise<void>
+	{
+		const preview = await AsciidocPreview.revive(
+			webview,
+			state,
+			this._contentProvider,
+			this._previewConfigurations,
+			this._logger,
+			this._topmostLineMonitor,
+			this._contributions);
 
-	  this.registerPreview(preview);
+		this.registerPreview(preview);
 	}
 
 	private getExistingPreview(
-	  resource: vscode.Uri,
-	  previewSettings: PreviewSettings
-	): AsciidocPreview | undefined {
-	  return this._previews.find((preview) =>
-	    preview.matchesResource(resource, previewSettings.previewColumn, previewSettings.locked));
+		resource: vscode.Uri,
+		previewSettings: PreviewSettings
+	): AsciidocPreview | undefined
+	{
+		return this._previews.find((preview) =>
+			preview.matchesResource(resource, previewSettings.previewColumn, previewSettings.locked));
 	}
 
 	private createNewPreview(
-	  resource: vscode.Uri,
-	  previewSettings: PreviewSettings
-	): AsciidocPreview {
-	  const preview = AsciidocPreview.create(
-	    resource,
-	    previewSettings.previewColumn,
-	    previewSettings.locked,
-	    this._contentProvider,
-	    this._previewConfigurations,
-	    this._logger,
-	    this._topmostLineMonitor,
-	    this._contributions);
+		resource: vscode.Uri,
+		previewSettings: PreviewSettings
+	): AsciidocPreview
+	{
+		const preview = AsciidocPreview.create(
+			resource,
+			previewSettings.previewColumn,
+			previewSettings.locked,
+			this._contentProvider,
+			this._previewConfigurations,
+			this._logger,
+			this._topmostLineMonitor,
+			this._contributions);
 
-	  this.setPreviewActiveContext(true);
-	  this._activePreview = preview;
-	  return this.registerPreview(preview);
+		this.setPreviewActiveContext(true);
+		this._activePreview = preview;
+		return this.registerPreview(preview);
 	}
 
 	private registerPreview(
-	  preview: AsciidocPreview
-	): AsciidocPreview {
-	  this._previews.push(preview);
+		preview: AsciidocPreview
+	): AsciidocPreview
+	{
+		this._previews.push(preview);
 
-	  preview.onDispose(() => {
-	    const existing = this._previews.indexOf(preview);
-	    if (existing === -1) {
-	      return;
-	    }
+		preview.onDispose(() =>
+		{
+			const existing = this._previews.indexOf(preview);
+			if (existing === -1)
+			{
+				return;
+			}
 
-	    this._previews.splice(existing, 1);
-	    if (this._activePreview === preview) {
-	      this.setPreviewActiveContext(false);
-	      this._activePreview = undefined;
-	    }
-	  });
+			this._previews.splice(existing, 1);
+			if (this._activePreview === preview)
+			{
+				this.setPreviewActiveContext(false);
+				this._activePreview = undefined;
+			}
+		});
 
-	  preview.onDidChangeViewState(({ webviewPanel }) => {
-	    disposeAll(this._previews.filter((otherPreview) => preview !== otherPreview && preview!.matches(otherPreview)));
-	    this.setPreviewActiveContext(webviewPanel.active);
-	    this._activePreview = webviewPanel.active ? preview : undefined;
-	  });
+		preview.onDidChangeViewState(({ webviewPanel }) =>
+		{
+			disposeAll(this._previews.filter((otherPreview) => preview !== otherPreview && preview!.matches(otherPreview)));
+			this.setPreviewActiveContext(webviewPanel.active);
+			this._activePreview = webviewPanel.active ? preview : undefined;
+		});
 
-	  return preview;
+		return preview;
 	}
 
-	private setPreviewActiveContext(value: boolean) {
-	  vscode.commands.executeCommand('setContext', AsciidocPreviewManager.asciidocPreviewActiveContextKey, value);
+	private setPreviewActiveContext(value: boolean)
+	{
+		vscode.commands.executeCommand('setContext', AsciidocPreviewManager.asciidocPreviewActiveContextKey, value);
 	}
 }
