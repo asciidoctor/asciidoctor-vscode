77d8700c8f1d118ce8a290996ddf958a3db44a7e
Daniel Mulholland <dan.mulholland@gmail.com>
2021-10-16 09:00:34 +1300

    Blacklist certain link addresses, make preview code for links similar to Markdown extension

--- src/AsciidoctorWebViewConverter.ts
+++ src/AsciidoctorWebViewConverter.ts
@@ -1,5 +1,22 @@
 const processor = require('@asciidoctor/core')()
 
+const BAD_PROTO_RE = /^(vbscript|javascript|data):/i
+const GOOD_DATA_RE = /^data:image\/(gif|png|jpeg|webp);/i
+
+/**
+ * Disallow blacklisted URL types following MarkdownIt and the
+ * VS Code Markdown extension
+ * @param   {String}  href   The link address
+ * @returns {boolean}        Whether the link is valid
+ */
+function isSchemeBlacklisted (href: string): boolean {
+  const hrefCheck = href.trim()
+  return !(BAD_PROTO_RE.test(hrefCheck) ? GOOD_DATA_RE.test(hrefCheck) : true)
+}
+
+/**
+ * Custom converter to support VS Code WebView security
+ */
 export class AsciidoctorWebViewConverter {
   baseConverter: any
 
@@ -7,13 +24,20 @@ export class AsciidoctorWebViewConverter {
     this.baseConverter = processor.Html5Converter.create()
   }
 
+  /**
+   Convert links to ensure WebView security for preview through use of data-href attribute
+   * @param node        Type of node in Asciidoctor AST
+   * @param transform   An optional string transform that hints at which transformation should be applied to this node
+   * @returns           Converted node
+   */
   convert (node, transform) {
     const nodeName = transform || node.getNodeName()
     if (nodeName === 'inline_anchor' && node.type === 'link') {
-      const id = node.id ? ` id="${node.id}"` : ''
-      const role = node.role ? ` class="${node.role}"` : ''
-      const title = node.title ? ` title="${node.title}"` : ''
-      return `<a href="${node.target}"${id}${role}${title} data-href="${node.target}">${node.text}</a>`
+      const href = isSchemeBlacklisted(node.target) ? '' : node.target
+      const id = node.hasAttribute('id') ? ` id="${node.id}"` : ''
+      const role = node.hasAttribute('role') ? ` class="${node.role}"` : ''
+      const title = node.hasAttribute('title') ? ` title="${node.title}"` : ''
+      return `<a href="${href}"${id}${role}${title} data-href="${href}">${node.text}</a>`
     }
     return this.baseConverter.convert(node, transform)
   }
