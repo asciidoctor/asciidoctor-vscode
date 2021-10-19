0ee5779a67bcdcd5c4c989d189c1b5a94001b9c1
Daniel Mulholland <dan.mulholland@gmail.com>
2021-10-19 20:57:57 +1300

    Satisfy linter

--- src/asciidoctorWebViewConverter.ts
+++ src/asciidoctorWebViewConverter.ts
@@ -12,8 +12,8 @@ const GOOD_DATA_RE = /^data:image\/(gif|png|jpeg|webp);/i
 function isSchemeBlacklisted (href: string): boolean {
   const hrefCheck = href.trim()
   if (BAD_PROTO_RE.test(hrefCheck)) {
-    // we still allow safe "data:image/" URI
-    return GOOD_DATA_RE.test(hrefCheck) ? false : true
+    // we still allow specific safe "data:image/" URIs
+    return !GOOD_DATA_RE.test(hrefCheck)
   }
   return false
 }
