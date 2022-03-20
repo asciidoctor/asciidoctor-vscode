ba6b644ecc2df4ac2f5837f74791457150a31254
Marie Flores <marieflores.contact@gmail.com>
2022-03-20 15:18:06 +0100

    Add code folding on conditional preprocessor directives
    fixes #552

--- src/test/foldingProvider.test.ts
+++ src/test/foldingProvider.test.ts
@@ -13,21 +13,21 @@ import { InMemoryDocument } from './inMemoryDocument'
 const testFileName = vscode.Uri.file('test.adoc')
 
 suite('asciidoc.FoldingProvider', () => {
-  test('Should not return anything for empty document', async () => {
-    const folds = await getFoldsForDocument('')
+  test('Should not return anything for empty document', () => {
+    const folds = getFoldsForDocument('')
     assert.strictEqual(folds.length, 0)
   })
 
-  test('Should not return anything for document without headers', async () => {
-    const folds = await getFoldsForDocument(`a
+  test('Should not return anything for document without headers', () => {
+    const folds = getFoldsForDocument(`a
 *b* afas
 a=b
 a`)
     assert.strictEqual(folds.length, 0)
   })
 
-  test('Should fold from header to end of document', async () => {
-    const folds = await getFoldsForDocument(`= a
+  test('Should fold from header to end of document', () => {
+    const folds = getFoldsForDocument(`= a
 
 == b
 
@@ -39,8 +39,8 @@ d`)
     assert.strictEqual(firstFold.end, 5)
   })
 
-  test('Should leave single newline before next header', async () => {
-    const folds = await getFoldsForDocument(`
+  test('Should leave single newline before next header', () => {
+    const folds = getFoldsForDocument(`
 == a
 x
 
@@ -52,8 +52,8 @@ y`)
     assert.strictEqual(firstFold.end, 3)
   })
 
-  test('Should collapse multiple newlines to single newline before next header', async () => {
-    const folds = await getFoldsForDocument(`
+  test('Should collapse multiple newlines to single newline before next header', () => {
+    const folds = getFoldsForDocument(`
 == a
 x
 
@@ -70,8 +70,8 @@ y`)
     assert.strictEqual(secondFold.end, 7)
   })
 
-  test('Should not collapse if there is no newline before next header', async () => {
-    const folds = await getFoldsForDocument(`= a
+  test('Should not collapse if there is no newline before next header', () => {
+    const folds = getFoldsForDocument(`= a
 x
 == b
 y`)
@@ -82,8 +82,66 @@ y`)
   })
 })
 
-async function getFoldsForDocument (contents: string) {
+test('Should fold from ifeval conditional beginning to its end', () => {
+  const folds = getFoldsForDocument(`ifeval::["{lang}" == "de"]
+Das ist mein Text.
+endif::[]`)
+  assert.strictEqual(folds.length, 1)
+  const firstFold = folds[0]
+  assert.strictEqual(firstFold.start, 0)
+  assert.strictEqual(firstFold.end, 2)
+})
+
+test('Should fold from ifndef conditional beginning to its end', () => {
+  const folds = getFoldsForDocument(`ifndef::env-github[]
+This content is not shown on GitHub.
+endif::[]`)
+  assert.strictEqual(folds.length, 1)
+  const firstFold = folds[0]
+  assert.strictEqual(firstFold.start, 0)
+  assert.strictEqual(firstFold.end, 2)
+})
+
+test('Should fold from ifdef multi conditionals beginning to its end', () => {
+  const folds = getFoldsForDocument(`ifeval::["{InstallOS}"=="Linux"]
+:install-os-linux:
+endif::[]
+ifeval::["{InstallOS}"=="Solaris"]
+:install-os-solaris:
+endif::[]
+ifeval::["{InstallOS}"=="Windows"]
+:install-os-windows:
+endif::[]
+
+ifdef::install-os-linux,install-os-solaris[]
+NOTE: We recommend that you install the library in the \`/opt/\` directory.
+endif::[]
+ifdef::install-os-windows[]
+NOTE: We recommend that you install the library in the \`C:\\Program Files\` directory.
+endif::[]`)
+  assert.strictEqual(folds.length, 5)
+  const firstFold = folds[0]
+  assert.strictEqual(firstFold.start, 0)
+  assert.strictEqual(firstFold.end, 2)
+})
+
+test('Should fold nested conditionals', () => {
+  const folds = getFoldsForDocument(`ifdef::foo[]
+foo1
+ifdef::bar[]
+bar
+endif::[]
+foo2
+endif::[]`)
+  assert.strictEqual(folds.length, 2, 'expecting 2 folds')
+  assert.deepStrictEqual(folds, [
+    new vscode.FoldingRange(2, 4, vscode.FoldingRangeKind.Region),
+    new vscode.FoldingRange(0, 6, vscode.FoldingRangeKind.Region),
+  ])
+})
+
+function getFoldsForDocument (contents: string) {
   const doc = new InMemoryDocument(testFileName, contents)
   const provider = new AsciidocFoldingProvider(createNewAsciidocEngine())
-  return await provider.provideFoldingRanges(doc, new vscode.CancellationTokenSource().token)
+  return provider.provideFoldingRanges(doc, new vscode.CancellationTokenSource().token)
 }
--- src/test/tableOfContentsProvider.test.ts
+++ src/test/tableOfContentsProvider.test.ts
@@ -13,21 +13,21 @@ import { createNewAsciidocEngine } from './engine'
 const testFileName = vscode.Uri.file('test.md')
 
 suite('asciidoc.TableOfContentsProvider', () => {
-  test('Lookup should not return anything for empty document', async () => {
+  test('Lookup should not return anything for empty document', () => {
     const doc = new InMemoryDocument(testFileName, '')
     const provider = new TableOfContentsProvider(createNewAsciidocEngine(), doc)
 
-    assert.strictEqual(await provider.lookup(''), undefined)
-    assert.strictEqual(await provider.lookup('foo'), undefined)
+    assert.strictEqual(provider.lookup(''), undefined)
+    assert.strictEqual(provider.lookup('foo'), undefined)
   })
 
-  test('Lookup should not return anything for document with no headers', async () => {
+  test('Lookup should not return anything for document with no headers', () => {
     const doc = new InMemoryDocument(testFileName, 'a *b*\nc')
     const provider = new TableOfContentsProvider(createNewAsciidocEngine(), doc)
 
-    assert.strictEqual(await provider.lookup(''), undefined)
-    assert.strictEqual(await provider.lookup('foo'), undefined)
-    assert.strictEqual(await provider.lookup('a'), undefined)
-    assert.strictEqual(await provider.lookup('b'), undefined)
+    assert.strictEqual(provider.lookup(''), undefined)
+    assert.strictEqual(provider.lookup('foo'), undefined)
+    assert.strictEqual(provider.lookup('a'), undefined)
+    assert.strictEqual(provider.lookup('b'), undefined)
   })
 })
