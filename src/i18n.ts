26d88bee30eeb5f22d8aae6930229dc30f50458c
Guillaume Grossetie <ggrossetie@gmail.com>
2022-07-15 18:38:32 +0200

    resolves #600 set a default locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,5 @@
 import * as nls from 'vscode-nls'
 
 // configure once
-nls.config({ messageFormat: nls.MessageFormat.file })()
+// set a default locale until https://github.com/microsoft/vscode-nls/commit/e3f36f5026867758173405adb4800ebc02eef631 is available in a release
+nls.config({ locale: 'en-US', messageFormat: nls.MessageFormat.file })()
