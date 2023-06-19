45302b52a57c8e3ffdd37cdca3a4f7145df7be23
Aurélien Pupier <apupier@redhat.com>
2023-06-19 18:41:44 +0200

    Search in attribute value during completion (#721)
    resolves #720
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ src/test/attributeRefCompletionProvider.test.ts
@@ -0,0 +1,62 @@
+import 'mocha'
+import * as vscode from 'vscode'
+import assert from 'assert'
+import { AttributeReferenceProvider } from '../features/attributeReferenceProvider'
+import { Position } from 'vscode'
+
+let root
+
+suite('Attribute ref CompletionsProvider', () => {
+  let createdFiles: vscode.Uri[] = []
+  setup(() => {
+    root = vscode.workspace.workspaceFolders[0].uri.fsPath
+  })
+  teardown(async () => {
+    for (const createdFile of createdFiles) {
+      await vscode.workspace.fs.delete(createdFile)
+    }
+    createdFiles = []
+  })
+  test('Should return attribute key defined in same file', async () => {
+    const fileToAutoComplete = vscode.Uri.file(`${root}/fileToAutoComplete-attributeRef.adoc`)
+    await vscode.workspace.fs.writeFile(fileToAutoComplete, Buffer.from(`:my-attribute-to-find-in-completion: dummy value
+`))
+    createdFiles.push(fileToAutoComplete)
+
+    const file = await vscode.workspace.openTextDocument(fileToAutoComplete)
+    const completionsItems = new AttributeReferenceProvider().provideCompletionItems(file, new Position(1, 0))
+    const filteredCompletionItems = completionsItems.filter((completionItem) => {
+      if ((completionItem.label as vscode.CompletionItemLabel)) {
+        return (completionItem.label as vscode.CompletionItemLabel).label === 'my-attribute-to-find-in-completion'
+      } else {
+        return false
+      }
+    })
+    const completionItem = filteredCompletionItems[0]
+    assert.deepStrictEqual((completionItem.label as vscode.CompletionItemLabel).description, 'dummy value')
+    assert.deepStrictEqual(completionItem.insertText, '{my-attribute-to-find-in-completion}')
+  })
+  test('Should return attribute key defined in same file corresponding to its value', async () => {
+    const fileToAutoComplete = vscode.Uri.file(`${root}/fileToAutoComplete-attributeRef.adoc`)
+    await vscode.workspace.fs.writeFile(fileToAutoComplete, Buffer.from(`:my-attribute-to-find-in-completion: dummy value
+dumm`))
+    createdFiles.push(fileToAutoComplete)
+
+    const file = await vscode.workspace.openTextDocument(fileToAutoComplete)
+    const completionsItems = new AttributeReferenceProvider().provideCompletionItems(file, new Position(1, 3))
+    const filteredCompletionItems = completionsItems.filter((completionItem) => (completionItem.label as vscode.CompletionItemLabel).label === 'my-attribute-to-find-in-completion')
+    const completionItem = filteredCompletionItems[0]
+    assert.deepStrictEqual((completionItem.label as vscode.CompletionItemLabel).description, 'dummy value')
+    assert.deepStrictEqual(completionItem.insertText, '{my-attribute-to-find-in-completion}')
+  })
+  test('Should return no completion when nothign corresponds', async () => {
+    const fileToAutoComplete = vscode.Uri.file(`${root}/fileToAutoComplete-attributeRef.adoc`)
+    await vscode.workspace.fs.writeFile(fileToAutoComplete, Buffer.from(`:my-attribute-to-find-in-completion: dummy value
+somethingVeryDifferent`))
+    createdFiles.push(fileToAutoComplete)
+
+    const file = await vscode.workspace.openTextDocument(fileToAutoComplete)
+    const completionsItems = new AttributeReferenceProvider().provideCompletionItems(file, new Position(1, 22))
+    assert.notStrictEqual(completionsItems.length, 0, 'There are Completion provided although none are expected.')
+  })
+})
