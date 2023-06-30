f907d867042b02dcc82de9ce8a7a84c61f0b2aaa
Aurélien Pupier <apupier@redhat.com>
2023-06-30 16:10:52 +0200

    WIP - Avoid illegal argument exception in toc provider
    provided test to reproduce the issue
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/tableOfContentsProvider.test.ts
+++ src/test/tableOfContentsProvider.test.ts
@@ -11,7 +11,7 @@ import { InMemoryDocument } from './inMemoryDocument'
 
 const testFileName = vscode.Uri.file('test.md')
 
-suite('asciidoc.TableOfContentsProvider', () => {
+suite.only('asciidoc.TableOfContentsProvider', () => {
   test('Lookup should not return anything for empty document', () => {
     const doc = new InMemoryDocument(testFileName, '')
     const provider = new TableOfContentsProvider(doc)
@@ -28,5 +28,46 @@ suite('asciidoc.TableOfContentsProvider', () => {
     assert.strictEqual(provider.lookup('foo'), undefined)
     assert.strictEqual(provider.lookup('a'), undefined)
     assert.strictEqual(provider.lookup('b'), undefined)
+  })
+
+  test('Toc should contain top level element from first line', () => {
+    const doc = new InMemoryDocument(testFileName, `= a section title
+
+content`)
+    const provider = new TableOfContentsProvider(doc)
+
+    assert.strictEqual(provider.getToc().length, 1)
+  })
+
+  suite('With includes', () => {
+    let createdFiles: vscode.Uri[] = []
+    let root: string
+    setup(() => {
+      root = vscode.workspace.workspaceFolders[0].uri.fsPath
+    })
+    teardown(async () => {
+      for (const createdFile of createdFiles) {
+        await vscode.workspace.fs.delete(createdFile)
+      }
+      createdFiles = []
+    })
+
+    test('Toc should contain top level element from first line', async () => {
+      const fileToHaveTOC = vscode.Uri.file(`${root}/fileToHaveTOC.adoc`)
+      await vscode.workspace.fs.writeFile(fileToHaveTOC, Buffer.from(`= a section title
+include::includedFile.adoc[]
+
+content`))
+      createdFiles.push(fileToHaveTOC)
+
+      const includedFile = vscode.Uri.file(`${root}/includedFile.adoc`)
+      await vscode.workspace.fs.writeFile(includedFile, Buffer.from(':dummy: dummy'))
+      createdFiles.push(includedFile)
+
+      const doc = await vscode.workspace.openTextDocument(fileToHaveTOC)
+      const provider = new TableOfContentsProvider(doc)
+
+      assert.strictEqual(provider.getToc().length, 1)
+    })
   })
 })
