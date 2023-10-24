2804351296662fc7ea2787f8767114976c743fbb
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2023-10-24 10:31:50 +0200

    resolves #805 fix web preview

--- src/features/asciidoctorConfig.ts
+++ src/features/asciidoctorConfig.ts
@@ -1,10 +1,6 @@
-import * as path from 'path'
 import * as vscode from 'vscode'
 import { Asciidoctor } from '@asciidoctor/core'
 import { AsciidoctorProcessor } from '../asciidoctorProcessor'
-import { getWorkspaceFolder } from '../util/workspace'
-
-const MAX_DEPTH_SEARCH_ASCIIDOCCONFIG = 100
 
 export interface AsciidoctorConfigProvider {
   activate(registry: Asciidoctor.Extensions.Registry, documentUri: vscode.Uri): Promise<void>;
@@ -46,49 +42,25 @@ export class AsciidoctorConfig implements AsciidoctorConfigProvider {
   }
 }
 
-async function exists (uri: vscode.Uri) {
-  try {
-    await vscode.workspace.fs.stat(uri)
-    return true
-  } catch (err) {
-    if (err && err.code === 'FileNotFound') {
-      return false
-    }
-    throw err
-  }
-}
-
 export async function getAsciidoctorConfigContent (documentUri: vscode.Uri): Promise<String | undefined> {
-  const workspaceFolder = getWorkspaceFolder(documentUri)
-  if (workspaceFolder === undefined) {
+  const asciidoctorConfigs = (await vscode.workspace.findFiles('**/.{asciidoctorconfig.adoc,asciidoctorconfig}', null))
+    .filter((uri) => {
+      const documentParentDirectory = documentUri.path.slice(0, documentUri.path.lastIndexOf('/'))
+      const asciidoctorConfigParentDirectory = uri.path.slice(0, uri.path.lastIndexOf('/'))
+      return documentParentDirectory.startsWith(asciidoctorConfigParentDirectory)
+    })
+    .sort((a, b) => a.path.localeCompare(b.path))
+  if (asciidoctorConfigs.length === 0) {
     return undefined
   }
-
-  const configContents: string[] = []
-  let currentFile: string = documentUri.fsPath
-  let increment = 0
-  while (currentFile !== undefined && currentFile !== workspaceFolder.uri.fsPath && increment < MAX_DEPTH_SEARCH_ASCIIDOCCONFIG) {
-    increment++
-    currentFile = path.dirname(currentFile)
-    configContents.push(await getConfigContent(currentFile, '.asciidoctorconfig.adoc'))
-    configContents.push(await getConfigContent(currentFile, '.asciidoctorconfig'))
+  const configContents = []
+  for (const asciidoctorConfig of asciidoctorConfigs) {
+    const asciidoctorConfigContent = new TextDecoder().decode(await vscode.workspace.fs.readFile(asciidoctorConfig))
+    const asciidoctorConfigParentDirectory = asciidoctorConfig.path.slice(0, asciidoctorConfig.path.lastIndexOf('/'))
+    configContents.push(`:asciidoctorconfigdir: ${asciidoctorConfigParentDirectory}\n\n${asciidoctorConfigContent.trim()}\n\n`)
   }
-
-  const configContentsOrderedAndFiltered = configContents
-    .filter((config) => config !== undefined)
-    .reverse()
-
-  if (configContentsOrderedAndFiltered.length > 0) {
-    return configContentsOrderedAndFiltered.join('\n\n')
-  }
-  return undefined
-}
-
-async function getConfigContent (folderPath: string, configFilename: string) {
-  const asciidoctorConfigUri = vscode.Uri.joinPath(vscode.Uri.file(folderPath), configFilename)
-  if (await exists(asciidoctorConfigUri)) {
-    const asciidoctorConfigContent = new TextDecoder().decode(await vscode.workspace.fs.readFile(asciidoctorConfigUri))
-    return `:asciidoctorconfigdir: ${folderPath}\n\n${asciidoctorConfigContent.trim()}\n\n`
+  if (configContents.length > 0) {
+    return configContents.join('\n\n')
   }
   return undefined
 }
