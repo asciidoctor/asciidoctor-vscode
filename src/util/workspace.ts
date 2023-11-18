3471441b744e53d9bc01080832eeaa765f129069
Guillaume Grossetie <ggrossetie@gmail.com>
2023-11-18 13:46:23 +0100

    Fix drive letter normalization
    Previously the whole path was matching and we basically applied toLowerCase on the path!

--- src/util/workspace.ts
+++ src/util/workspace.ts
@@ -1,11 +1,13 @@
 import vscode, { Uri, WorkspaceFolder } from 'vscode'
 import os from 'os'
 
+const driveLetterRx = /(?<=^\/)([A-Z])(?=:\/)/
+
 export function getWorkspaceFolder (uri: Uri): WorkspaceFolder | undefined {
   const workspaceFolder = vscode.workspace.getWorkspaceFolder(uri)
   if (workspaceFolder && os.platform() === 'win32') {
     return {
-      uri: workspaceFolder.uri.with({ path: workspaceFolder.uri.path.replace(/^\/([A-Z]):.*/, (driverLetter) => driverLetter.toLowerCase()) }),
+      uri: workspaceFolder.uri.with({ path: workspaceFolder.uri.path.replace(driveLetterRx/, (driverLetter) => driverLetter.toLowerCase()) }),
       name: workspaceFolder.name,
       index: workspaceFolder.index,
     }
@@ -17,7 +19,7 @@ export function getWorkspaceFolders (): WorkspaceFolder[] | undefined {
   return vscode.workspace.workspaceFolders?.map((workspaceFolder) => {
     if (os.platform() === 'win32') {
       return {
-        uri: workspaceFolder.uri.with({ path: workspaceFolder.uri.path.replace(/^\/([A-Z]):.*/, (driverLetter) => driverLetter.toLowerCase()) }),
+        uri: workspaceFolder.uri.with({ path: workspaceFolder.uri.path.replace(driveLetterRx, (driverLetter) => driverLetter.toLowerCase()) }),
         name: workspaceFolder.name,
         index: workspaceFolder.index,
       }
@@ -38,7 +40,7 @@ export function getDefaultWorkspaceFolderUri (): Uri | undefined {
   const workspaceFolders = getWorkspaceFolders()
   let workspaceUri = workspaceFolders[0].uri
   if (os.platform() === 'win32') {
-    workspaceUri = workspaceUri.with({ path: workspaceUri.path.replace(/^\/([A-Z]):.*/, (driverLetter) => driverLetter.toLowerCase()) })
+    workspaceUri = workspaceUri.with({ path: workspaceUri.path.replace(driveLetterRx, (driverLetter) => driverLetter.toLowerCase()) })
   }
   return workspaceUri
 }
