e164c6b780b11c5b1647f71e8f445cb07656f703
Leonardo Pavan Rocha <leonardo.rocha.pavan@gmail.com>
2024-04-23 13:43:16 +0200

    fix asciidoc preview when antora is using version: true
    Fixes https://github.com/asciidoctor/asciidoctor-vscode/issues/829

--- src/features/antora/antoraSupport.ts
+++ src/features/antora/antoraSupport.ts
@@ -25,6 +25,9 @@ export class AntoraConfig {
     const path = uri.path
     this.contentSourceRootPath = path.slice(0, path.lastIndexOf('/'))
     this.contentSourceRootFsPath = ospath.dirname(uri.fsPath)
+    if (config.version === true) {
+      config.version = ''
+    }
   }
 }
 
