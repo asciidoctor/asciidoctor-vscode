6052d5ad8b00f86aec1a7c3ccd7286d3e491d74b
Alain Dresse <alain.dresse@twipemobile.com>
2024-05-27 13:37:00 +0200

    removed extraneous console.log

--- src/features/antora/antoraSupport.ts
+++ src/features/antora/antoraSupport.ts
@@ -25,7 +25,6 @@ export class AntoraConfig {
     const path = uri.path
     this.contentSourceRootPath = path.slice(0, path.lastIndexOf('/'))
     this.contentSourceRootFsPath = ospath.dirname(uri.fsPath)
-    console.log('version: ' + (config.version || 'missing'))
     if (config.version === true || !(config.version)) {
       config.version = ''
     }
